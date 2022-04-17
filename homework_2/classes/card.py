ed0d19e6f01d6c68d6d268712f0ed41b86800dfc
kongri <konstantin@gribanov.net>
2022-04-17 12:37:16 +0300

    card __str__ updated

--- homework_2/classes/card.py
+++ homework_2/classes/card.py
@@ -2,7 +2,8 @@ from random import randint, sample, choice
 
 
 class Card:
-    def __init__(self):
+    def __init__(self, username):
+        self.username = username
         self.fields = self.generate_fields()
 
     def generate_fields(self):
@@ -10,23 +11,33 @@ class Card:
 
         for x in range(3):
             items = sample(range(9), 5)
-            line = [randint(1, 90) for _ in range(9)]
+            line = sorted([randint(1, 90) for _ in range(9)])
             new_line = []
             for i in range(9):
                 if i in items:
                     new_line.append(line[i])
                 else:
-                    new_line.append("_")
+                    new_line.append(" ")
             field[x] = new_line
         return field
 
+    def _build_str(self):
+        half_header = "-" * int((64 - len(self.username)) / 2)
+        header = f"\n{half_header} {self.username} {half_header}\n"
+        footer = "-" * 66
+        out = header
+        for i in self.fields:
+            line = "\t".join(str(l) for l in self.fields[i])
+            out += f"\n{line}\n"
+        out += f"\n{footer}\n"
+        return out
+
     def __str__(self):
-        return f"{self.fields}"
+        return self._build_str()
 
 
 def main():
-    card = Card()
-    print(card)
+    card = Card("Vasily Pupkin")
     print(card)
 
 
--- homework_2/test_card.py
+++ homework_2/test_card.py
@@ -3,31 +3,31 @@ import pytest
 
 
 def test_create_card():
-    card = Card()
+    card = Card("User")
     assert isinstance(card, Card)
 
 
 def test_generate_fields():
-    card = Card()
+    card = Card("User")
     field = card.generate_fields()
     assert isinstance(field, dict)
 
 
 def test_dict_contains_tree_lists():
-    card = Card()
+    card = Card("User")
     field = card.generate_fields()
     assert len(field) == 3
 
 
 def test_each_list_of_dict_has_length_9():
-    card = Card()
+    card = Card("User")
     field = card.generate_fields()
     for i in field:
         assert len(field[i]) == 9
 
 
 def test_each_list_of_dict_contains_five_digits():
-    card = Card()
+    card = Card("User")
     field = card.generate_fields()
     for i in field:
-        assert field[i].count("_") == 4
+        assert field[i].count(" ") == 4
