173bb8948c6fa0e07377af190e110a5842dc1004
kongri <konstantin@gribanov.net>
2022-04-22 14:17:00 +0300

    Куфвьу гвфеув

--- homework_4/main.py
+++ homework_4/main.py
@@ -7,7 +7,7 @@ from database import get_session
 
 def main():
     session = get_session()
-    # user_crud.create_user(session, "user3@test.com", "password123")
+    # user_crud.create_user(session, "test.user2@test.com", "password123")
     user = user_crud.get_user(session, "test.user2@test.com")
     # print(user.password)
     # user_crud.delete_user(session, "test.user2@test.com")
